79505b1d264f74099ba94826d2a789dd696f2e3f
Arief Darmawan <ariefda@hotmail.com>
2015-07-25 19:38:38 +0700

    update test with database entry

--- parallel_test.go
+++ parallel_test.go
@@ -2,14 +2,30 @@ package parallel
 
 import (
 	"fmt"
+	"github.com/eaciit/database/base"
+	"github.com/eaciit/database/mongodb"
 	"github.com/eaciit/toolkit"
+	"runtime"
+	"strconv"
 	"testing"
 	"time"
 )
 
 var e error
 
+const (
+	pworkerCount = 100
+	pnumCount    = 20000
+)
+
+func init() {
+	fmt.Println("Initiate Env")
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	fmt.Println("")
+}
+
 func Test1(t *testing.T) {
+	t.Skip()
 	fmt.Println("Test 1 - Generate x Number")
 	numCount := 1000
 	workerCount := 100
@@ -38,5 +54,89 @@ func Test1(t *testing.T) {
 		fmt.Printf("Test OK in %v \n\n", pr.Duration)
 	} else {
 		fmt.Printf("Test Fail want %d  got %d and has %d records\n", total, want, pr.Success)
+	}
+}
+
+func TestDb1(t *testing.T) {
+	t0 := time.Now()
+	fmt.Println("Test 2a - Insert Data (No Parallel)")
+	numCount := pnumCount
+	idxs := make([]interface{}, numCount)
+	for i := 1; i <= numCount; i++ {
+		idxs[i-1] = i
+	}
+	conn := mongodb.NewConnection("localhost:27888", "", "", "ectest")
+	e = conn.Connect()
+	if e != nil {
+		t.Error("Unable to connect to Database | " + e.Error())
+	}
+	defer conn.Close()
+	success := 0
+	fmt.Printf("Insert %d data with no pool \n", numCount)
+	for _, jb := range idxs {
+		j := jb.(int)
+		user := toolkit.M{}
+		userid := "User " + strconv.Itoa(j)
+		user.Set("_id", userid)
+		user.Set("fullname", "User "+strconv.Itoa(j))
+		user.Set("email", "user"+strconv.Itoa(j)+"@email.com")
+		//_, _, e = conn.Query().From("ORMUsers").Save(user).Run()
+		_, e := conn.Execute("appusers", toolkit.M{"find": toolkit.M{"_id": userid}, "operation": base.DB_INSERT, "data": user})
+		if e == nil {
+			success++
+		}
+	}
+
+	if success == numCount {
+		fmt.Printf("Test OK in %v \n\n", time.Since(t0))
+	} else {
+		fmt.Printf("Test Fail has %d records in %v \n\n", success, time.Since(t0))
+	}
+}
+
+func TestDb2(t *testing.T) {
+	fmt.Println("Test 2b - Insert Data (Parallel)")
+	numCount := pnumCount
+	workerCount := pworkerCount
+	idxs := make([]interface{}, numCount)
+	want := 0
+	for i := 1; i <= numCount; i++ {
+		idxs[i-1] = i
+		want += i
+	}
+
+	conn := mongodb.NewConnection("localhost:27888", "", "", "ectest")
+	e = conn.Connect()
+	if e != nil {
+		t.Error("Unable to connect to Database | " + e.Error())
+	}
+	defer conn.Close()
+	fmt.Printf("Insert %d data within %d pools \n", numCount, workerCount)
+	pr := RunParallel(idxs, workerCount, func(js <-chan interface{}, rs chan<- *toolkit.Result) {
+		for jb := range js {
+			j := jb.(int)
+			user := toolkit.M{}
+			userid := "User " + strconv.Itoa(j)
+			user.Set("_id", userid)
+			user.Set("fullname", "User "+strconv.Itoa(j))
+			user.Set("email", "user"+strconv.Itoa(j)+"@email.com")
+			r := new(toolkit.Result)
+			//_, _, e = conn.Query().From("ORMUsers").Save(user).Run()
+			_, e := conn.Execute("appusers", toolkit.M{"find": toolkit.M{"_id": userid}, "operation": base.DB_INSERT, "data": user})
+			if e == nil {
+				r.Status = toolkit.Status_OK
+				r.Data = user
+			} else {
+				r.Status = toolkit.Status_NOK
+				r.Message = "Error ID " + strconv.Itoa(j) + " " + e.Error()
+			}
+			rs <- r
+		}
+	})
+
+	if pr.Success == numCount {
+		fmt.Printf("Test OK in %v \n\n", pr.Duration)
+	} else {
+		fmt.Printf("Test Fail has %d records. \nErrors\n%v\nIn %v \n\n", pr.Success, toolkit.JsonString(pr.Errors[0]), pr.Duration)
 	}
 }
